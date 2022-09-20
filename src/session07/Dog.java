5c5b18b2e7ff18944256ecfb45a8c8277125dc28
ericgardell <38889337+ericgardell@users.noreply.github.com>
2022-09-20 10:19:27 -0400

    updated initializing constructor of dog class to use custum default values for the fields.

--- src/session07/Dog.java
+++ src/session07/Dog.java
@@ -7,17 +7,17 @@ public class Dog {
     double Weight;
     String Gender;
 
-    public Dog(String Breed, int Age, String Name, double Weight, String Gender) {
-        this.Breed   = Breed;
-        this.Age     = Age;
-        this.Name    = Name;
-        this.Weight  = Weight;
-        this.Gender  = Gender;
+    public Dog() {
+        Breed   = "Doodle";
+        Age     = 3;
+        Name    = "Barkley";
+        Weight  = 25;
+        Gender  = "Male";
     }
 
     public static void main(String[] args) {
-        Dog dog1 = new Dog("doodle", 3, "Marley", 25, "Male");
-        Dog dog2 = new Dog("lab", 5, "Chloe", 45, "Female");
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
         System.out.println(dog1.Name+" is a "+dog1.Gender+" "+dog1.Breed+" that is "+dog1.Age+" years old and weighs "+dog1.Weight+" lbs.");
         System.out.println(dog2.Name+" is a "+dog2.Gender+" "+dog2.Breed+" that is "+dog2.Age+" years old and weighs "+dog2.Weight+" lbs.");
     }
