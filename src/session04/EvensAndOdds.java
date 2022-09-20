8d054f1f9b86119bb5a4ad36c34759652480a18c
ericgardell <38889337+ericgardell@users.noreply.github.com>
2022-09-20 09:05:42 -0400

    Converted all session directories to packages and created Dog class in session07

--- src/session04/EvensAndOdds.java
+++ src/session04/EvensAndOdds.java
@@ -1,4 +1,4 @@
-package activities;
+package session04;
 
 import java.util.Scanner;
 
---
+++ src/session07/Dog.java
@@ -0,0 +1,18 @@
+package session07;
+
+public class Dog {
+    String Breed;
+    int Age;
+    String Name;
+    double Weight;
+    String Gender;
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        System.out.println(dog1.Name);
+        System.out.println(dog2.Age);
+    }
+}
+
+
