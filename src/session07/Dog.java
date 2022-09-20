86bd8f684aa95b3076b47e347071fb60ec3f2ff5
ericgardell <38889337+ericgardell@users.noreply.github.com>
2022-09-20 11:30:59 -0400

    updated Dog class to include an override for toString() method

--- src/session07/Dog.java
+++ src/session07/Dog.java
@@ -1,21 +1,10 @@
 package session07;
 
 public class Dog {
-    String Breed;
-    int Age;
-    String Name;
-    double Weight;
-    String Gender;
+    private String Breed;
+    private int Age;
+    private String Name;
 
-    // Default Constructor
-    public Dog() {
-        Breed = "Doodle";
-        Age = 3;
-        Name = "DogName";
-        Weight = 25;
-        Gender = "Gender";
-
-    }
     // Initializing Constructor
     public Dog(String Name, int Age) {
         this.Name    = Name;
@@ -23,15 +12,49 @@ public class Dog {
     }
 
     public Dog(String Breed, int Age, String Name) {
-        this("Pluto", 5);
+        this(Name, Age);
         this.Breed = Breed;
     }
 
+    public void birthday() {
+        this.Age ++;
+        System.out.println("Congrats! "+this.Name+" turned "+this.Age+" years old");
+    }
+
+    public void breedSwap(String newBreed) {
+        this.Breed = newBreed;
+        System.out.println(this.Name+" is now a "+this.Breed);
+    }
+
+    // accessor method for Age
+    public int getAge(){
+        return this.Age;
+    }
+
+    // mutator method for Age
+    public void setAge(int newAge) {
+        this.Age = newAge;
+    }
+
+    public void setName(String newName) {
+        this.Name = newName;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog[ name:"+this.Name+" | Breed:"+this.Breed+" | Age:"+this.Age+" ]";
+    }
+
     public static void main(String[] args) {
-        Dog dog1 = new Dog("doodle", 3, "Marley"); //, 25, "Male"
+        Dog dog1 = new Dog("doodle", 3, "Marly"); //, 25, "Male"
         Dog dog2 = new Dog("lab", 5, "Chloe"); //, 45, "Female"
-        System.out.println(dog1.Name+" is a "+dog1.Gender+" "+dog1.Breed+" that is "+dog1.Age+" years old and weighs "+dog1.Weight+" lbs.");
-        System.out.println(dog2.Name+" is a "+dog2.Gender+" "+dog2.Breed+" that is "+dog2.Age+" years old and weighs "+dog2.Weight+" lbs.");
+        dog1.setName("Marley");
+        System.out.println(dog1);
+        System.out.println(dog2);
+        dog1.birthday();
+        dog2.breedSwap("Labradoodle");
+        System.out.println(dog1.getAge());
+        dog2.setAge(6);
     }
 }
 
